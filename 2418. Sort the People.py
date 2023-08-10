5a2877249b2ccb08d75713a029fc52b32da20915
Doniyor401 <doniyortursunboyev000@bk.ru>
2023-08-10 22:25:25 +0500

    4-HW 4-masalalar

---
+++ 2418. Sort the People.py
@@ -0,0 +1,3 @@
+class Solution:
+  def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+    return [height for _, height in sorted([(height, name) for name, height in zip(names, heights)], reverse=True)]
